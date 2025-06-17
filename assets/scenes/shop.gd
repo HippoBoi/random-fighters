a3fbb4b7a2553d11ca4b3474b7829cb818373764
unknown <tagrifavc@gmail.com>
2025-06-17 17:09:04 -0400

    items list

--- assets/scenes/shop.gd
+++ assets/scenes/shop.gd
@@ -1,1 +1,8 @@
 extends Control
+
+func _ready() -> void:
+	var itemTemplate = $Shop/ItemsContainer/itemTemplate;
+	
+	for i in Constants.items:
+		var newItem = itemTemplate.duplicate();
+		newItem.name = i.name;
--- assets/scripts/constants.gd
+++ assets/scripts/constants.gd
@@ -11,6 +11,29 @@ enum GameModes {
 	Hippo_Capture
 }
 
-const item = {
-	
-}
+const items = [
+	{
+		"name": "pato fakiu",
+		"stats": {
+			"hp": 10,
+			"dmg": 5
+		},
+		"texture": "res://assets/textures/61SLCkmPE8L._AC_UF894,1000_QL80_DpWeblab_.jpg"
+	},
+	{
+		"name": "kris deltarune",
+		"stats": {
+			"speed": 10,
+			"dmg": 15
+		},
+		"texture": "res://assets/textures/Kris_Overworld_b.webp"
+	},
+	{
+		"name": "ralsei deltarun",
+		"stats": {
+			"speed": 20,
+			"armor": 10,
+		},
+		"texture": "res://assets/textures/Ralsei_overworld.webp"
+	}
+];
