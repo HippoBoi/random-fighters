11db1a2be3d611ab5fbe22d22e8b23cd27ce4f80
Andy <tagrifavc@gmail.com>
2025-07-29 04:21:39 -0400

    added snowman NPC to snowmen map

---
+++ assets/maps/snowmen.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var snowmanBlack = null;
+var snowmanWhite = null;
+
+func _ready() -> void:
+	if (has_node("snowmanBlack")):
+		snowmanBlack = get_node("snowmanBlack");
+	if (has_node("snowmanWhite")):
+		snowmanWhite = get_node("snowmanWhite");
+	
+	if not (snowmanBlack and snowmanWhite):
+		print("[WARNING] failed to load snowmen");
+		return;
+	
+	_setupSnowmen();
+
+func _setupSnowmen():
+	snowmanBlack.rpc("showUI");
+	snowmanWhite.rpc("showUI");
--- assets/npcs/snowman/snowman.gd
+++ assets/npcs/snowman/snowman.gd
@@ -1,3 +1,288 @@
-extends Node3D
+extends CharacterBody3D
 
 @export var team = -1;
+@export var maxHp = 450.0;
+@export var hp = 450.0;
+@export var baseArmor = 7.0;
+@export var baseDmg = 17.0;
+@export var baseAttackRange = 4.0;
+@export var baseAttackSpeed = 4.0;
+@export var baseSpeed = 4.0;
+@export var cooldownReduction = 0;
+var shield = 0;
+
+const BASIC_ATTACK_COOLDOWN = 300;
+const CHARACTER_NAME = "SERVER";
+
+var dmgOffset = 0;
+var dmg = 0;
+var armorOffset = 0;
+var armor = 0;
+var attackRangeOffset = 0;
+var attackRange = 0;
+var attackSpeedOffset = 0;
+var attackSpeed = 0;
+var speedOffset = 0;
+var speed = 0;
+var speedMultiplier = 0;
+
+var timer = 0;
+var rayOrigin = Vector3();
+var rayEnd = Vector3();
+var moveTo = Vector3();
+var forceMoveTo = Vector3();
+var forceMoveSpeed = 5.0;
+var bufferedMoveTo = Vector3();
+var lastPos = Vector3();
+var target = null;
+var showingUIs = false;
+var basicAttacking = false;
+var basicDamageDealt = false;
+var basicAttackTimer = 0;
+var basicAttackMoment = BASIC_ATTACK_COOLDOWN * 0.9;
+var onAction = false;
+var overrideBasic = false;
+var usingSecondary = false;
+var usingTertiary = false;
+var primaryTimer = 0;
+var secondaryTimer = 0;
+var tertiaryTimer = 0;
+var ultiTimer = 0;
+var usingUlti = false;
+var ultiTarget = null;
+var playingUltiSound = false;
+var bufferedTarget = null;
+var bufferedInput = null;
+
+var stunned = false;
+var stunnedParts = null;
+var stunTimer = 0;
+var dead = false;
+var inFog = false;
+var enemyTeamVision = false;
+var fogInstances = [];
+
+var lives = 0;
+var level = 1;
+var xp = 0;
+var tokens = 0;
+var respawnTimer = 0;
+var assistedInKill = [];
+
+var basicAnimList = ["basic_01", "basic_02"];
+var basicAnimPos = 0;
+
+var moveToDirection: Vector3;
+
+@onready var camera = get_viewport().get_camera_3d();
+@onready var charModel = $snowman_armature;
+@onready var animPlayer = $AnimationPlayer;
+
+func _ready() -> void:
+	set_multiplayer_authority(-1);
+	PlayerFunc.setup(self);
+
+func rotateChar(newPos) -> void:
+	var direction = (newPos - global_position);
+	direction.y = 0;
+	direction = direction.normalized();
+
+	var targetRotation = atan2(direction.x, direction.z);
+
+	var curRotation = rotation.y;
+	var shortestAngle = lerp_angle(curRotation, targetRotation, 1.0);
+
+	var tween = get_tree().create_tween();
+	tween.tween_property(
+		self,
+		"rotation",
+		Vector3(rotation.x, shortestAngle, rotation.z),
+		0.18
+	);
+
+func _physics_process(delta: float) -> void:
+	PlayerFunc.updateGlobally(self, delta);
+	
+	if (bufferedMoveTo and moveTo == null):
+		moveTo = bufferedMoveTo;
+		bufferedMoveTo = null;
+	
+	# if (moveTo):
+		#P layerFunc.moveChar(self, delta, moveTo);
+	
+	# move_and_slide();
+	
+	# handle animations
+	if (onAction or basicAttacking):
+		return;
+	
+	if (velocity != Vector3.ZERO):
+		if not (animPlayer.current_animation == "run"):
+			animPlayer.play("run");
+	else:
+		if not (animPlayer.is_playing() and animPlayer.current_animation != "run"):
+			animPlayer.play("idle");
+	
+func updateHealthSize():
+	var UILoaded = has_node("CharacterUI");
+	if not (UILoaded):
+		return;
+	
+	var charUI = get_node("CharacterUI");
+	var healthBar = charUI.get_node("HealthUI/SubViewport/emptyBar/healthBar");
+	var shieldBar = charUI.get_node("HealthUI/SubViewport/emptyBar/shieldBar");
+	healthBar.scale.x = hp / maxHp;
+	shieldBar.scale.x = shield / maxHp;
+
+@rpc("call_local", "any_peer", "reliable")
+func showUI():
+	var charUI = preload("res://assets/characters/character_ui.tscn").instantiate();
+	var healthBar = charUI.get_node("HealthUI/SubViewport/emptyBar/healthBar");
+	charUI.get_node("PlayerName/SubViewport/Label").text = "";
+	healthBar.color = Color(0.769, 0.17, 0.182);
+	add_child(charUI);
+	
+	charUI.global_position.y = 4.0;
+
+@rpc("call_local", "any_peer", "reliable")
+func syncTarget(_target):
+	target = _target;
+
+@rpc("call_local", "any_peer", "reliable")
+func syncHealth(curHealth, _shield, damaged = false, attackerId: String = ""):
+	hp = curHealth;
+	PlayerFunc.updateHealthSize(self, damaged);
+	
+	var speedAmount = randf_range(2.0, 4.0);
+	speedOffset += speedAmount;
+	speedOffset = clamp(speedOffset, 0, 20.0);
+	
+	if not (attackerId.is_empty()):
+		var oldAttackerPos = assistedInKill.find(attackerId);
+		if (oldAttackerPos != -1):
+			assistedInKill.remove_at(oldAttackerPos);
+		
+		assistedInKill.append(attackerId);
+	
+@rpc("any_peer")
+func syncPosition(newPos):
+	global_position = newPos;
+
+@rpc("call_local", "any_peer")
+func syncRotation(newPos):
+	rotateChar(newPos);
+
+@rpc("call_local", "any_peer")
+func syncStats(_speedOffset):
+	speedOffset = _speedOffset;
+
+@rpc("call_local", "any_peer", "reliable")
+func killHippo():
+	if (dead):
+		return;
+	
+	dead = true;
+	visible = false;
+	moveTo = global_position;
+	PlayerFunc.syncMovement(self);
+	
+	var scene = get_parent().get_parent().get_parent(); # lol
+	if (scene.name != "Game"):
+		print("[WARNING]: game node not found");
+		return;
+	
+	var invertedAssistsArray: Array = assistedInKill;
+	invertedAssistsArray.reverse();
+	
+	var index = 0;
+	var winnerTeam = -1;
+	for playerId in assistedInKill:
+		if (scene.name != "Game"):
+			print("[WARNING]: game node not found");
+			return;
+		
+		var character: CharacterBody3D = scene.get_character_by_id(playerId);
+		if (character):
+			if (index != 0):
+				break;
+			
+			character.level += 1;
+			winnerTeam = character.team;
+			
+		index += 1;
+	
+	if (winnerTeam == -1):
+		print("[WARNING]: couldn't find team that killed hippo");
+		return;
+	
+	for playerId in Server.playersInfo:
+		var playerData = Server.playersInfo[playerId];
+		var character = scene.get_character_by_id(str(playerData.playerID));
+		
+		if (character.team == winnerTeam):
+			character.tokens += 5;
+			character.hp += character.maxHp / 1.25;
+			character.hp = clamp(character.hp, 0, character.maxHp);
+			
+			var newParticles = preload("res://assets/effects/hippo_buff_particles.tscn").instantiate();
+			character.add_child(newParticles);
+			
+			newParticles.get_node("buffExplosion").emitting = true;
+			newParticles.get_node("sparkParticle").emitting = true;
+			newParticles.get_node("buffParticles").emitting = true;
+	
+	var particles = preload("res://assets/characters/dead_particles.tscn").instantiate();
+	get_parent().add_child(particles);
+	
+	particles.global_position = global_position + Vector3(0, 2, 0);
+	particles.get_node("pointyParts").emitting = true;
+	particles.get_node("spiralParts").emitting = true;
+
+@rpc("any_peer")
+func syncStun(_isStunned, _stunDuration):
+	stunned = _isStunned;
+	stunTimer = _stunDuration;
+
+@rpc("any_peer")
+func syncSlow(_slowAmount):
+	speedMultiplier -= _slowAmount;
+	speedMultiplier = clamp(speedMultiplier, 0.0, 1.0);
+
+@rpc("any_peer")
+func syncBufferedInputs(_moveTo = null, _target = null):
+	if (_moveTo):
+		bufferedMoveTo = _moveTo;
+	if (_target):
+		bufferedTarget = _target;
+
+@rpc("any_peer", "call_local")
+func simulateForcedMove(newPos, moveSpeed = 7.0, _global_pos = Vector3.ZERO):
+	if (newPos == null):
+		forceMoveTo = _global_pos;
+		return;
+	
+	forceMoveSpeed = moveSpeed;
+	forceMoveTo = newPos;
+
+@rpc("call_local", "any_peer")
+func syncParticles(effect: String, offset: Vector3 = Vector3(0, 2, 0)):
+	var path = "res://assets/effects/%s.tscn" % effect;
+	var effectNode = load(path);
+	var effectInstance = effectNode.instantiate();
+	
+	add_child(effectInstance);
+	effectInstance.global_position = global_position + offset;
+
+@rpc("any_peer")
+func syncRespawn(newHp: float, newPos: Vector3):
+	global_position = newPos;
+	hp = newHp;
+	dead = false;
+	visible = true;
+
+@rpc("call_local")
+func showChatText(newText):
+	print("SERVER: ", newText);
+
+func onCollision():
+	pass;
