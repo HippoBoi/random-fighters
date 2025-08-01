4b08f0fa35f880993faf6b6805fafe0bd3d68245
Andy <tagrifavc@gmail.com>
2025-07-31 23:00:55 -0400

    fix: player icon rotation

--- assets/characters/characterIcon.gd
+++ assets/characters/characterIcon.gd
@@ -1,9 +1,14 @@
 extends Sprite3D
 
+@export var size = 3.0;
+
 var parent;
 
 func _ready() -> void:
 	parent = get_parent();
+	global_rotation.z = -44.7;
+	global_scale(Vector3(size, size, size));
+	visible = true;
 
 func _physics_process(_delta: float) -> void:
 	global_position = parent.global_position;
--- assets/scripts/constants.gd
+++ assets/scripts/constants.gd
@@ -25,14 +25,14 @@ const items = [
 	},
 	{
 		"name": "Heaven Sword",
-		"price": 0,
+		"price": 15,
 		"stats": {
 			"dmg": 4,
 		},
 		"texture": "res://assets/textures/items/heaven_sword.png"
 	},{
 		"name": "Heaven Shield",
-		"price": 0,
+		"price": 15,
 		"stats": {
 			"hp": 25,
 			"armor": 10
