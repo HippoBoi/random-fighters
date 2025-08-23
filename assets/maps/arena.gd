816b6dca17c4b1e397a245adbc4795bf5ac621cd
Andy <tagrifavc@gmail.com>
2025-08-23 02:56:23 -0400

    update spawn positions on arena

--- assets/maps/arena.gd
+++ assets/maps/arena.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
+var closingZone: Node3D = null;
+var closingZoneName: String = "";
 var fireCircle: Node3D = null;
 var bigFireCircle: Node3D = null;
-var fireScale = 10.0;
+var fireScale = 15.0;
 var fireRatio = 1.68;
+var minFireScale = 2.35;
 var timer = 0;
 var hurryTimer = 0;
 
-var minFireScale = 2.35;
+var updatedSpawns = false;
+
+var playersOutOfZone = [];
 
 func _ready() -> void:
 	set_multiplayer_authority(1);
@@ -21,11 +26,11 @@ func _ready() -> void:
 		var possibleZones = $closingZones.get_children();
 		possibleZones.shuffle();
 		
-		var closingZone = possibleZones[0];
+		closingZone = possibleZones[0];
 		if (closingZone.name == "pos1"):
 			minFireScale = 2.6;
 		
-		rpc("syncPosition", closingZone.global_position, minFireScale);
+		rpc("syncPosition", closingZone.global_position, minFireScale, closingZone.name);
 	
 	fireCircle.scale = Vector3(fireScale * fireRatio, 1.5, fireScale * fireRatio);
 	bigFireCircle.scale = Vector3(fireScale, 1.0, fireScale);
@@ -33,30 +38,50 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if (is_multiplayer_authority()):
 		timer += delta;
-		if (timer >= 11):
+		if (timer >= 15):
 			timer = 0;
-			hurryTimer = 0.3;
+			hurryTimer = 0.5;
 			rpc("syncParameters", fireScale, hurryTimer);
 	
 	if (hurryTimer > 0):
 		hurryTimer -= delta;
 		hurryTimer = clamp(hurryTimer, 0, 1);
+	
+	if (fireScale < 7.0 and not updatedSpawns):
+		var blackTeamPos: Vector3;
+		var whiteTeamPos: Vector3;
+		
+		if (closingZoneName == "pos1"):
+			whiteTeamPos = Vector3(-0.091, 0.592, -8.055);
+			blackTeamPos = Vector3(-0.091, 0.592, 6.966);
+		if (closingZoneName == "pos2"):
+			whiteTeamPos = Vector3(7.909, 0.592, 23.945);
+			blackTeamPos = Vector3(-8.091, 0.592, 23.945);
+		if (closingZoneName == "pos3"):
+			whiteTeamPos = Vector3(7.909, 0.592, -22.945);
+			blackTeamPos = Vector3(-8.091, 0.592, -22.945);
+		
+		$spawnLocations/whiteTeam.position = whiteTeamPos;
+		$spawnLocations/blackTeam.position = blackTeamPos;
+		
+		updatedSpawns = true;
 
 func _physics_process(delta: float) -> void:
 	fireCircle.scale = Vector3(fireScale * fireRatio, 1.5, fireScale * fireRatio);
 	bigFireCircle.scale = Vector3(fireScale, 1.0, fireScale);
 	
-	fireScale -= (delta + hurryTimer) * 0.5;
+	fireScale -= (delta + hurryTimer) * 0.15;
 	fireScale = clamp(fireScale, minFireScale, 50);
 	
 	if (Engine.get_physics_frames() % 90 == 0 and is_multiplayer_authority()):
 		rpc("syncParameters", fireScale, hurryTimer);
 
 @rpc("call_local", "any_peer", "reliable")
-func syncPosition(_position: Vector3, _minFireScale: float):
+func syncPosition(_position: Vector3, _minFireScale: float, _closingZoneName):
 	fireCircle.global_position = _position;
 	bigFireCircle.global_position = _position + Vector3(0, -0.035, 0);
 	minFireScale = _minFireScale;
+	closingZoneName = _closingZoneName;
 
 @rpc("call_local", "any_peer", "reliable")
 func syncParameters(_fireScale: float, _hurryTimer: float):
