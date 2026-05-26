342528549c7d8b6a7f8e50ebb8641b8925bbf31e
Andy <tagrifavc@gmail.com>
2026-05-25 20:02:34 -0400

    add grass to lake map

--- assets/scenes/choose_mode.gd
+++ assets/scenes/choose_mode.gd
@@ -6,7 +6,7 @@ var playerId: int;
 var alreadySelected = [];
 var selectedGameMode = "Unknown";
 var selectedMode = false;
-var forceMode = "free_for_all";
+var forceMode = "";
 
 var blackTeamWins = 0;
 var whiteTeamWins = 0;
